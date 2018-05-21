0ef3e81619c4fda44bcfc95ada790ad5f1d911a0
Alex Perrotta <alexandra.perrotta@gmail.com>
2018-05-20 21:12:38 -0400

    added promisify

--- dns_lookup/index.js
+++ dns_lookup/index.js
@@ -1,5 +1,6 @@
 const readline = require('readline');
 const dns = require('dns');
+const util = require('util'); // add this in order to use promisify
 
 // Create the interface
 const rl = readline.createInterface({
@@ -7,21 +8,39 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+
+// promisify
+// send the function, making dns.lookup into a promisify to use it elsewhere
+const lookup = util.promisify(dns.lookup);  
+ 
+
 // Prompt, then wait for user to type a domain name
 rl.question('Domain name: ', (domain) => {
   rl.close();
 
-  // Do the DNS lookup
-  dns.lookup(domain, (err, address) => {
-
-    if (err) {
-      // If there's an error, print it and
-      // exit the program (via `return`).
-      console.log(err.message);
-      return;
-    }
-
-    // Otherwise, print the IP address.
+// using a promise, gets the domain and then logs it to the console
+lookup(domain)
+  .then((address) => {
     console.log(`IP address: ${address}`);
   })
+  .catch((err) => {
+    return console.log(err);
+  });
+
+
+// alternate way
+
+//   // Do the DNS lookup
+//   dns.lookup(domain, (err, address) => {
+
+//     if (err) {
+//       // If there's an error, print it and
+//       // exit the program (via `return`).
+//       console.log(err.message);
+//       return;
+//     }
+
+//     // Otherwise, print the IP address.
+//     console.log(`IP address: ${address}`);
+//   })
 });
